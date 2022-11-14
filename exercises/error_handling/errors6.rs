6d3a1f44f511157bbbf2499c28adbe5e057c288b
22050214-fuxinyuan <xinyuanfu1998@gmail.com>
2022-11-14 16:00:12 +0800

    finish the error option generics trait. they are hard ones. and finish the  quiz 3

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -8,33 +8,53 @@
 
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
+// #[derive(PartialEq, Debug)]
+// enum ParsePosNonzeroError {
+//     Creation(CreationError),
+//     ParseInt(ParseIntError)
+// }
+
+// impl ParsePosNonzeroError {
+//     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
+//         ParsePosNonzeroError::Creation(err)
+//     }
+//     // TODO: add another error conversion function here.
+//     // fn from_parseint...
+// }
+
+// fn parse_pos_nonzero(s: &str)
+//     -> Result<PositiveNonzeroInteger, ParsePosNonzeroError>
+// {
+//     // TODO: change this to return an appropriate error instead of panicking
+//     // when `parse()` returns an error.
+//     let x: i64 = s.parse().unwrap();
+//     PositiveNonzeroInteger::new(x)
+//         .map_err(ParsePosNonzeroError::from_creation)
+// }
+
 #[derive(PartialEq, Debug)]
 enum ParsePosNonzeroError {
     Creation(CreationError),
-    ParseInt(ParseIntError)
+    ParseInt(ParseIntError),
 }
 
 impl ParsePosNonzeroError {
     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
-    // TODO: add another error conversion function here.
-    // fn from_parseint...
+
+    fn from_parse_int(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
-fn parse_pos_nonzero(s: &str)
-    -> Result<PositiveNonzeroInteger, ParsePosNonzeroError>
-{
-    // TODO: change this to return an appropriate error instead of panicking
-    // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
-    PositiveNonzeroInteger::new(x)
-        .map_err(ParsePosNonzeroError::from_creation)
+fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
+    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parse_int)?;
+    PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
 
 // Don't change anything below this line.
